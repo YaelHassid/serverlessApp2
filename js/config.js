bb880ec26c02fec38964be752b8b224437e68eb3
Yael Hassid <yaelhassid@h-MacBook-Pro-sl-Yael.local>
2024-06-05 16:37:37 +0300

    add user pool2

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-2_9JzcIELAX'
-        userPoolClientId: '1k6ida6kq71e6a57ftqg8d0tko'
+        userPoolClientId: '6i6juo793pche6rhb7i0jo70rn'
         region: 'us-west-2'
     },
     api: {
