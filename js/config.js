fa47b6e28a7b2d22192ffb121788448284a40c76
Yael Hassid <yaelhassid@h-MacBook-Pro-sl-Yael.local>
2024-06-05 16:20:06 +0300

    add user pool

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_9JzcIELAX'
+        userPoolClientId: '1k6ida6kq71e6a57ftqg8d0tko'
+        region: 'us-west-2'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
