206f77f448a12eda55e1135b7ee716210dd2901e
Yael Hassid <yaelhassid@h-MacBook-Pro-sl-Yael.local>
2024-06-09 16:16:52 +0300

    added api invoke

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-west-2'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+        invokeUrl: 'https://5j9gvwrj1l.execute-api.us-west-2.amazonaws.com/prod' 
+}
 };
